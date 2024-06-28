c625265a9fd79a97001fcd9d5908ec9aaf787ace
avokadoen <akselhallajs@gmail.com>
2024-06-28 16:26:27 +0200

    Make testing scheduler optional for now
    Due to #175

--- src/main.zig
+++ src/main.zig
@@ -35,13 +35,21 @@ pub const tracy_alloc = @import("tracy_alloc.zig");
 pub const ezby = @import("ezby.zig");
 
 test {
+    const scheduler_testing = blk: {
+        const options = @import("ecez_options");
+
+        break :blk options.scheduler_testing;
+    };
+    if (scheduler_testing) {
+        _ = @import("scheduler.zig");
+    }
+
     _ = @import("archetype_container.zig");
     _ = @import("binary_tree.zig");
     _ = @import("ezby.zig");
     _ = @import("iterator.zig");
     _ = @import("opaque_archetype.zig");
     _ = @import("storage.zig");
-    _ = @import("scheduler.zig");
     _ = @import("meta.zig");
     _ = @import("storage_edit_queue.zig");
 }
